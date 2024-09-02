5b3ec546e5cd29ea514bcff0daf6b2585e35f462
lucas-braga <olalucasbraga@gmail.com>
2024-09-02 07:13:29 -0300

    finished the v1

--- app/page.tsx
+++ app/page.tsx
@@ -103,7 +103,7 @@ export default function Home() {
         <div className="flex items-start justify-between">
           <div className="max-w-2xl">
             <div className="inline-block bg-green-100 text-green-800 px-3 py-1 rounded-full text-sm mb-4">
-              Vamos levar o seu negócio para o próximo nível
+              Vamos juntos para o próximo nível?
             </div>
             <h1 className="text-4xl font-bold mb-4">
               Transforme os resultados da sua empresa usando
@@ -345,40 +345,50 @@ export default function Home() {
           </div>
         </Card>
       </Card>
+      {/* Last CTA */}
       <div className="flex flex-col lg:flex-row gap-8 p-8">
         <div className="lg:w-1/2 space-y-6">
           <h2 className="text-4xl font-bold">
-            Turn 7 stops to 1 stop.
+            <svg
+              className="inline-block w-8 h-8 mr-2"
+              viewBox="0 0 24 24"
+              fill="#00A211"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M12 3L4 15h16L12 3z" />
+            </svg>
+            Performance{" "}
+            <svg
+              className="inline-block w-8 h-8 mr-2"
+              viewBox="0 0 24 24"
+              fill="#FF0000"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M12 21L4 9h16L12 21z" />
+            </svg>
+            Custo
           </h2>
           <p className="text-gray-600">
-            Say goodbye to traditional old-fashioned
-            fulfillment. With Ecomflow fulfillment you are
-            able to scale worldwide based on actual demand
-            instead of sales forecasts without having to
-            wait 60 days with 7 stops.
+            Não perca tempo com processos burocráticos e
+            desnecessários. Com a nossa IA, você terá um
+            controle total sobre seus processos e custos.
           </p>
           <div className="flex gap-8">
             <div>
               <p className="text-5xl font-bold text-[#00A211]">
-                65+
+                7+
               </p>
-              <p className="text-gray-600">Countries</p>
+              <p className="text-gray-600">
+                Anos de experiência
+              </p>
             </div>
             <div>
               <p className="text-5xl font-bold text-[#00A211]">
-                98%
+                83
               </p>
-              <p className="text-gray-600">
-                On-Time Delivery
-              </p>
+              <p className="text-gray-600">NPS médio</p>
             </div>
           </div>
-          <Button
-            className="text-[#00A211] hover:text-[#008E0F] p-0"
-            variant="link"
-          >
-            Pricing & benefits →
-          </Button>
         </div>
         <div className="lg:w-1/2">
           <div className="flex gap-4">
@@ -485,16 +495,17 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <Card className="bg-[#0A2F1F] text-white p-8 relative overflow-hidden">
-        <div className="relative z-10">
+      {/* Contact Form */}
+      <Card className="bg-[#0A2F1F] text-white p-8 relative overflow-hidden flex space-x-8">
+        <div className="relative z-10 w-1/2">
           <h2 className="text-4xl font-bold mb-4">
-            Any Questions?
+            Alguma dúvida?
           </h2>
           <p className="mb-6">
-            In case you still have any questions, feel free
-            reach out to us by filling in the form below,
-            and our team will get back to you as soon as
-            possible.
+            Se ainda tem alguma dúvida, entre em contato
+            conosco preenchendo o formulário abaixo, e nossa
+            equipe entrará em contato com você o mais rápido
+            possível. possible.
           </p>
           <form
             onSubmit={handleSubmit}
@@ -502,14 +513,14 @@ export default function Home() {
           >
             <div>
               <Label htmlFor="name" className="text-white">
-                Name
+                Nome
               </Label>
               <Input
                 id="name"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
                 className="bg-white text-black"
-                placeholder="Jane Smith"
+                placeholder="Nome"
               />
             </div>
             <div>
@@ -524,7 +535,7 @@ export default function Home() {
                   e: React.ChangeEvent<HTMLInputElement>
                 ) => setEmail(e.target.value)}
                 className="bg-white text-black"
-                placeholder="jane@ecomflow.com"
+                placeholder="Email"
               />
             </div>
             <div>
@@ -532,14 +543,14 @@ export default function Home() {
                 htmlFor="message"
                 className="text-white"
               >
-                Message
+                Mensagem
               </Label>
               <Textarea
                 id="message"
                 value={message}
                 onChange={(e) => setMessage(e.target.value)}
                 className="bg-white text-black"
-                placeholder="Enter your question..."
+                placeholder="Digite sua mensagem..."
                 rows={4}
               />
             </div>
@@ -547,14 +558,40 @@ export default function Home() {
               type="submit"
               className="w-full bg-[#00A211] hover:bg-[#008E0F]"
             >
-              Submit
+              Enviar
             </Button>
           </form>
         </div>
-        <div className="absolute top-0 right-0 w-full h-full opacity-10">
-          <div className="w-full h-full bg-dot-pattern" />
-        </div>
+        <div className="w-1/2 bg-red-500">placeholder</div>
       </Card>
+      {/* Footer */}
+      <footer className="flex justify-between items-center py-4 px-8 mt-10">
+        <p className="text-sm text-gray-600">
+          © 2023 Novos Robôs. Todos os direitos reservados.
+        </p>
+        <div className="flex space-x-4">
+          <a href="#" aria-label="LinkedIn">
+            <svg
+              className="w-6 h-6 text-gray-600 hover:text-gray-800"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
+            </svg>
+          </a>
+          <a href="#" aria-label="Instagram">
+            <svg
+              className="w-6 h-6 text-gray-600 hover:text-gray-800"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
+            </svg>
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }
